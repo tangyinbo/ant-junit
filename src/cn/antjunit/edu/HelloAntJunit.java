8e9491cb666865eeacc2f9fce5d8cb921f38d171
tangyinbo <canton_cowboy@super_pc>
2014-08-17 21:50:16 +0800

    ant 生成doc和zip

--- src/cn/antjunit/edu/HelloAntJunit.java
+++ src/cn/antjunit/edu/HelloAntJunit.java
@@ -1,9 +1,40 @@
 package cn.antjunit.edu;
 
+/**
+ * 
+ * @ClassName: HelloAntJunit
+ * @Description: 用于测试javadoc
+ * @author Tangyinbo
+ * @date 2014-8-17 下午9:06:23
+ * 
+ */
 public class HelloAntJunit {
-	public int add(int a,int b){
-		return a+b;
+	/**
+	 * 我谁测试add
+	 * @Title: add
+	 * @Description: 加法
+	 * @param @param a
+	 * @param @param b
+	 * @param @return
+	 * @return int
+	 * @author Tangyinbo
+	 * @date 2014-8-17 下午9:06:36
+	 * @throws
+	 */
+	public int add(int a, int b) {
+		return a + b;
 	}
-	public void thowException(){
+
+	/**
+	 * 我是测试异常
+	 * @Title: thowException
+	 * @Description: 测试抛出异常
+	 * @param
+	 * @return void
+	 * @author Tangyinbo
+	 * @date 2014-8-17 下午9:06:51
+	 * @throws
+	 */
+	public void thowException() {
 	}
 }
